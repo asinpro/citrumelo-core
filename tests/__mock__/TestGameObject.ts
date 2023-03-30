20ecc0ec348106a0cfbe67691c6fd83375a591a0
aXeL <axelg@Gamtop>
2023-03-30 11:22:08 +0400

    GameObject tests

---
+++ tests/__mock__/TestGameObject.ts
@@ -0,0 +1,8 @@
+import { GameObject } from '../../src'
+
+export default class TestGameObject extends GameObject {
+    public number_value!: number
+    public bool_value!: boolean
+    public str_value!: string
+    public array_value!: any[]
+}
